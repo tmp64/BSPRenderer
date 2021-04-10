c112dc70423c39b74974f59bf35071f9797e49ab
tmp64 <tmp32x2@gmail.com>
2021-04-10 21:52:21 +0700

    Update bmplib

--- src/playground/src/bmplib.hpp
+++ src/playground/src/bmplib.hpp
@@ -2,6 +2,7 @@
 #include <cstring>
 #include <iostream>
 #include <fstream>
+#include <cassert>
 
 class byteArray {
 public:
@@ -18,7 +19,7 @@ public:
 		bytes = new char[this->size]();
 		int temp = num;
 		for (int i = 0; i < size; i++) {
-			bytes[i] = temp % 256;
+			bytes[i] = static_cast<char>(temp % 256);
 			temp >>= 8;
 		}
 	}
@@ -53,6 +54,9 @@ public:
 		memcpy(bytes, r.bytes, size);
 	}
 	inline byteArray& operator=(byteArray&& r) noexcept {
+	    if(this == &r){
+	        return *this;
+	    }
 		delete[] bytes;
 		bytes = r.bytes;
 		size = r.size;
@@ -61,10 +65,14 @@ public:
 		return *this;
 	}
 	inline byteArray& operator=(const byteArray& r) {
-		size = r.size;
+		if(this == &const_cast<byteArray&>(r)){
+            return *this;
+		}
+	    size = r.size;
 		delete[] bytes;
 		bytes = new char[size];
 		memcpy(bytes, r.bytes, size);
+		return *this;
 	}
 	inline byteArray operator+(const byteArray& rhs) const{
 		byteArray n(*this);
@@ -77,7 +85,7 @@ struct Color {
 	int red;
 	int green;
 	int blue;
-	inline Color(int r = 0, int g = 0, int b = 0) {
+	explicit inline Color(int r = 0, int g = 0, int b = 0) {
 		red = r;
 		green = g;
 		blue = b;
@@ -114,7 +122,8 @@ class bitMapImage {
 	int koef;
 public:
 	inline bitMapImage(int x, int y, Color* colors = nullptr, int size = 0) {
-		this->bfType.size = 2;
+		koef = 0;
+	    this->bfType.size = 2;
 		w = x;
 		h = y;
 		delete[] this->bfType.bytes;
@@ -142,11 +151,11 @@ public:
 		this->pixelBuf.bytes = new char[this->pixelBuf.size]();
 		iLoveColorTable(colors, size);
 	}
-	void setPixel(int x, int y, int colorIndex);
-	void setPixel(int x, int y, Color c);
-	void saveToFile(const char* filename);
+	void setPixel([[maybe_unused]] int x, [[maybe_unused]] int y, [[maybe_unused]] int colorIndex);
+    [[maybe_unused]] void setPixel(int x, int y, Color c);
+	void saveToFile([[maybe_unused]] const char* filename);
 private:
-	inline byteArray* getBytes() {
+    [[maybe_unused]] inline byteArray* getBytes() {
 		return &pixelBuf;
 	}
 	inline byteArray fullFile()
@@ -172,19 +181,21 @@ private:
 		ans += pixelBuf;
 		return ans;
 	}
-	void iLoveColorTable(Color* colors = nullptr, int size = 0);
+	void iLoveColorTable([[maybe_unused]] [[maybe_unused]] Color* colors = nullptr, [[maybe_unused]] [[maybe_unused]] int size = 0);
 };
 
-void bitMapImage<32>::setPixel(int x, int y, Color c) {
+template<>
+[[maybe_unused]] void bitMapImage<32>::setPixel(int x, int y, Color c) {
 	if (x >= w || y >= h) {
 		throw std::invalid_argument("Can't set this pixel");
 	}
-	pixelBuf.bytes[y * w * 4 + x * 4 + 2] = c.red;
-	pixelBuf.bytes[y * w * 4 + x * 4 + 1] = c.green;
-	pixelBuf.bytes[y * w * 4 + x * 4] = c.blue;
+	pixelBuf.bytes[y * w * 4 + x * 4 + 2] = static_cast<char>(c.red);
+	pixelBuf.bytes[y * w * 4 + x * 4 + 1] = static_cast<char>(c.green);
+	pixelBuf.bytes[y * w * 4 + x * 4] = static_cast<char>(c.blue);
 	pixelBuf.bytes[y * w * 4 + x * 4 + 3] = 0;
 }
 
+template<>
 void bitMapImage<32>::saveToFile(const char* filename) {
 	std::ofstream out(filename, std::ios::out | std::ios::binary);
 	byteArray sourceFile = fullFile();
@@ -192,22 +203,25 @@ void bitMapImage<32>::saveToFile(const char* filename) {
 	out.close();
 }
 
-void bitMapImage<32>::iLoveColorTable(Color* colors, int size) {
-	this->colorTbl = byteArray(4, 0x00FF0000) + byteArray(4, 0xFF000000) + byteArray(4, 0x0000FF00);
+template<>
+void bitMapImage<32>::iLoveColorTable([[maybe_unused]] Color* colors, [[maybe_unused]] int size) {
+	this->colorTbl = byteArray(4, 0x00FF0000) + byteArray(4, static_cast<int>(0xFF000000)) + byteArray(4, 0x0000FF00);
 	this->bfOffBits = byteArray(4, 66);
 	this->bfSize = byteArray(4, 66 + static_cast<int>(ceil(this->w * this->h * (static_cast<double>(32) / 8))) + this->koef);
 }
 
-void bitMapImage<24>::setPixel(int x, int y, Color c) {
+template<>
+[[maybe_unused]] void bitMapImage<24>::setPixel(int x, int y, Color c) {
 	if (x >= w || y >= h) {
 		throw std::invalid_argument("Can't set this pixel");
 	}
 	int koef1 = (koef / h) * y;
-	pixelBuf.bytes[y * w * 3 + x * 3 + 2 + koef1] = c.red;
-	pixelBuf.bytes[y * w * 3 + x * 3 + 1 + koef1] = c.green;
-	pixelBuf.bytes[y * w * 3 + x * 3 + koef1] = c.blue;
+	pixelBuf.bytes[y * w * 3 + x * 3 + 2 + koef1] = static_cast<char>(c.red);
+	pixelBuf.bytes[y * w * 3 + x * 3 + 1 + koef1] = static_cast<char>(c.green);
+	pixelBuf.bytes[y * w * 3 + x * 3 + koef1] = static_cast<char>(c.blue);
 }
 
+template<>
 void bitMapImage<24>::saveToFile(const char* filename) {
 	std::ofstream out(filename, std::ios::out | std::ios::binary);
 	byteArray sourceFile = fullFile();
@@ -215,13 +229,15 @@ void bitMapImage<24>::saveToFile(const char* filename) {
 	out.close();
 }
 
-void bitMapImage<24>::iLoveColorTable(Color* colors, int size) {
+template<>
+void bitMapImage<24>::iLoveColorTable([[maybe_unused]] Color* colors,[[maybe_unused]] int size) {
 	this->colorTbl = byteArray();
 	this->bfOffBits = byteArray(4, 54);
 	this->bfSize = byteArray(4, 54 + static_cast<int>(ceil(this->w * this->h * (static_cast<double>(24) / 8))) + this->koef);
 }
 
-void bitMapImage<16>::setPixel(int x, int y, Color c) {
+template<>
+[[maybe_unused]] void bitMapImage<16>::setPixel(int x, int y, Color c) {
 	if (x >= w || y >= h) {
 		throw std::invalid_argument("Can't set this pixel");
 	}
@@ -229,16 +245,16 @@ void bitMapImage<16>::setPixel(int x, int y, Color c) {
 	c.red = std::min(c.red, 31);
 	c.green = std::min(c.green, 63);
 	c.blue = std::min(c.blue, 31);
-	//TODO: хуйня полная, поправить
 	int t1 = c.red << 3;
 	int t11 = t1 + (c.green >> 3);
-	pixelBuf.bytes[y * w * 2 + x * 2 + 1 + koef1] = t11;
+	pixelBuf.bytes[y * w * 2 + x * 2 + 1 + koef1] = static_cast<char>(t11);
 	int tfuck = (c.green >> 3) << 3;
 	int t2 = (c.green - tfuck) << 5;
 	int t21 = t2 + c.blue;
-	pixelBuf.bytes[y * w * 2 + x * 2 + koef1] = t21;
+	pixelBuf.bytes[y * w * 2 + x * 2 + koef1] = static_cast<char>(t21);
 }
 
+template<>
 void bitMapImage<16>::saveToFile(const char* filename) {
 	std::ofstream out(filename, std::ios::out | std::ios::binary);
 	byteArray sourceFile = fullFile();
@@ -246,20 +262,23 @@ void bitMapImage<16>::saveToFile(const char* filename) {
 	out.close();
 }
 
-void bitMapImage<16>::iLoveColorTable(Color* colors, int size) {
+template<>
+void bitMapImage<16>::iLoveColorTable([[maybe_unused]] Color* colors, [[maybe_unused]] int size) {
 	this->colorTbl = byteArray(4, 0b0000011111100000) + byteArray(4, 0b1111100000000000) + byteArray(4, 0b0000000000011111);
 	this->bfOffBits = byteArray(4, 66);
 	this->bfSize = byteArray(4, 66 + static_cast<int>(ceil(this->w * this->h * (static_cast<double>(16) / 8))) + this->koef);
 }
 
+template<>
 void bitMapImage<8>::setPixel(int x, int y, int colorIndex) {
 	if (x >= w || y >= h) {
 		throw std::invalid_argument("Can't set this pixel");
 	}
 	int koef1 = (koef / h) * y;
-	pixelBuf.bytes[y * w + x + koef1] = colorIndex;
+	pixelBuf.bytes[y * w + x + koef1] = static_cast<char>(colorIndex);
 }
 
+template<>
 void bitMapImage<8>::saveToFile(const char* filename) {
 	std::ofstream out(filename, std::ios::out | std::ios::binary);
 	byteArray sourceFile = fullFile();
@@ -267,6 +286,7 @@ void bitMapImage<8>::saveToFile(const char* filename) {
 	out.close();
 }
 
+template<>
 void bitMapImage<8>::iLoveColorTable(Color* colors, int size) {
 	if (colors == nullptr)
 		throw std::invalid_argument("Giving nullptr is baaaaad. You shouldn't give nullptr because it's baaaaad, Mkay?");
@@ -280,12 +300,13 @@ void bitMapImage<8>::iLoveColorTable(Color* colors, int size) {
 	this->bfSize = byteArray(4, 54 + 256 * 4 + static_cast<int>(ceil(this->w * this->h * (static_cast<double>(8) / 8))) + this->koef);
 }
 
+template<>
 void bitMapImage<4>::setPixel(int x, int y, int colorIndex) {
 	if (x >= w || y >= h) {
 		throw std::invalid_argument("Can't set this pixel");
 	}
 	int koef1 = (koef / h) * y;
-	char num = pixelBuf.bytes[y * static_cast<int>(std::ceil(static_cast<double>(w) / 2)) + x / 2 + koef1];
+	unsigned char num = pixelBuf.bytes[y * static_cast<int>(std::ceil(static_cast<double>(w) / 2)) + x / 2 + koef1];
 	if (x % 2 == 0) {
 		num &= 0b00001111;
 		num += (colorIndex & 0b00001111) << 4;
@@ -294,9 +315,10 @@ void bitMapImage<4>::setPixel(int x, int y, int colorIndex) {
 		num &= 0b11110000;
 		num += (colorIndex & 0b00001111);
 	}
-	pixelBuf.bytes[static_cast<int>(y * std::ceil(static_cast<double>(w) / 2)) + x / 2 + koef1] = num;
+	pixelBuf.bytes[static_cast<int>(y * std::ceil(static_cast<double>(w) / 2)) + x / 2 + koef1] = static_cast<char>(num);
 }
 
+template<>
 void bitMapImage<4>::saveToFile(const char* filename) {
 	std::ofstream out(filename, std::ios::out | std::ios::binary);
 	byteArray sourceFile = fullFile();
@@ -304,6 +326,7 @@ void bitMapImage<4>::saveToFile(const char* filename) {
 	out.close();
 }
 
+template<>
 void bitMapImage<4>::iLoveColorTable(Color* colors, int size) {
 	if (colors == nullptr)
 		throw std::invalid_argument("Giving nullptr is baaaaad. You shouldn't give nullptr because it's baaaaad, Mkay?");
@@ -317,12 +340,13 @@ void bitMapImage<4>::iLoveColorTable(Color* colors, int size) {
 	this->bfSize = byteArray(4, 54 + 16 * 4 + static_cast<int>(ceil(this->w * this->h * (static_cast<double>(4) / 8))) + this->koef);
 }
 
+template<>
 void bitMapImage<1>::setPixel(int x, int y, int colorIndex) {
 	if (x >= w || y >= h) {
 		throw std::invalid_argument("Can't set this pixel");
 	}
 	int koef1 = (koef / h) * y;
-	char num = pixelBuf.bytes[y * static_cast<int>(std::ceil(static_cast<double>(w) / 8)) + x / 8 + koef1];
+	unsigned char num = pixelBuf.bytes[y * static_cast<int>(std::ceil(static_cast<double>(w) / 8)) + x / 8 + koef1];
 	switch (x % 8)
 	{
 	case 0:
@@ -358,9 +382,10 @@ void bitMapImage<1>::setPixel(int x, int y, int colorIndex) {
 		num += (colorIndex & 0b00000001);
 		break;
 	}
-	pixelBuf.bytes[y * static_cast<int>(std::ceil(static_cast<double>(w) / 8)) + x / 8 + koef1] = num;
+	pixelBuf.bytes[y * static_cast<int>(std::ceil(static_cast<double>(w) / 8)) + x / 8 + koef1] = static_cast<char>(num);
 }
 
+template<>
 void bitMapImage<1>::saveToFile(const char* filename) {
 	std::ofstream out(filename, std::ios::out | std::ios::binary);
 	byteArray sourceFile = fullFile();
@@ -368,6 +393,7 @@ void bitMapImage<1>::saveToFile(const char* filename) {
 	out.close();
 }
 
+template<>
 void bitMapImage<1>::iLoveColorTable(Color* colors, int size) {
 	if (colors == nullptr)
 		throw std::invalid_argument("Giving nullptr is baaaaad. You shouldn't give nullptr because it's baaaaad, Mkay?");
@@ -382,16 +408,22 @@ void bitMapImage<1>::iLoveColorTable(Color* colors, int size) {
 }
 
 template <int C>
-void bitMapImage<C>::setPixel(int x, int y, int colorIndex){
-	static_assert(C && !C, "Giving any other bit value then 1,4,8,16,24,32 is baaaaad. You shouldn't give any other bit value then 1,4,8,16,24,32 because it's baaaaad, Mkay?");
+void bitMapImage<C>::setPixel([[maybe_unused]] int x, [[maybe_unused]] int y, [[maybe_unused]] int colorIndex){
+    fprintf(stderr, "%d is not a valid bit count\n", C);
+    assert(false);
+    //static_assert(false, "Giving any other bit value then 1,4,8,16,24,32 is baaaaad. You shouldn't give any other bit value then 1,4,8,16,24,32 because it's baaaaad, Mkay?");
 }
 
 template <int C>
-void bitMapImage<C>::saveToFile(const char* filename) {
-	static_assert(C && !C, "Giving any other bit value then 1,4,8,16,24,32 is baaaaad. You shouldn't give any other bit value then 1,4,8,16,24,32 because it's baaaaad, Mkay?");
+void bitMapImage<C>::saveToFile([[maybe_unused]] const char* filename) {
+    fprintf(stderr, "%d is not a valid bit count\n", C);
+	assert(false);
+    //static_assert(false, "Giving any other bit value then 1,4,8,16,24,32 is baaaaad. You shouldn't give any other bit value then 1,4,8,16,24,32 because it's baaaaad, Mkay?");
 }
 
 template <int C>
-void bitMapImage<C>::iLoveColorTable(Color* colors, int size) {
-	static_assert(C && !C, "Giving any other bit value then 1,4,8,16,24,32 is baaaaad. You shouldn't give any other bit value then 1,4,8,16,24,32 because it's baaaaad, Mkay?");
+void bitMapImage<C>::iLoveColorTable([[maybe_unused]] Color* colors, [[maybe_unused]] int size) {
+    fprintf(stderr, "%d is not a valid bit count\n", C);
+    assert(false);
+	//assert(false, "Giving any other bit value then 1,4,8,16,24,32 is baaaaad. You shouldn't give any other bit value then 1,4,8,16,24,32 because it's baaaaad, Mkay?");
 }
